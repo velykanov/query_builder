690b551ae874456e5109ee0b20a6596562605b7a
Nikita Velykanov <nikitavelykanov@gmail.com>
2019-08-28 20:18:27 +0300

    Initial: Restructure methods calls

--- field.py
+++ field.py
@@ -2,7 +2,8 @@ import decimal
 
 
 class Field:
-    _not_implemented_method = NotImplementedError('implement method in child')
+    _unsupported_operand = "unsupported operand type(s) for {}: '{}' and '{}'"
+    _unsupported_unary_operand = "unsupported operand type(s) for {} on type {}"
 
     def __init__(self, name, alias=None):
         self.name = name
@@ -10,52 +11,98 @@ class Field:
         self._fields = {name: name}
 
     def __add__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '+',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __sub__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '-',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __rsub__(self, other):
         return self.__sub__(other)
 
     def __and__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '&',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __rand__(self, other):
         return self.__and__(other)
 
     def __or__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '|',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __ror__(self, other):
         return self.__or__(other)
 
     def __lt__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '<',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __le__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '<=',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __gt__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '>',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __ge__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '>=',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __ne__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '!=',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __eq__(self, other):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_operand.format(
+            '==',
+            type(self).__name__,
+            type(other).__name__,
+        ))
 
     def __neg__(self):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_unary_operand.format(
+            '-',
+            type(self).__name__,
+        ))
 
     def __bool__(self):
-        raise self._not_implemented_method
+        raise TypeError(self._unsupported_unary_operand.format(
+            'bool',
+            type(self).__name__,
+        ))
 
     def __repr__(self):
         return "<{} '{}'>".format(type(self).__name__, self.name)
@@ -69,24 +116,23 @@ class BigInt(Field):
     _min = -9223372036854775808
     _max = 9223372036854775807
 
-    def __add__(self, other):
+    def __general_operation(self, other, operand):
         if isinstance(other, Field):
             name = '{}_{}'.format(self.name, other.name)
-            value = '{} + {}'.format(self._fields[self.name], other.name)
+            value = '{} {} {}'.format(self._fields[self.name], operand, other.name)
         elif isinstance(other, (int, float, decimal.Decimal)):
             name = self.name
-            value = '{} + {}'.format(self._fields[self.name], other)
+            value = '{} {} {}'.format(self._fields[self.name], operand, other)
         else:
-            raise TypeError(
-                "unsupported operand type(s) for +: '{}' and '{}'".format(
-                    type(self).__name__, type(other).__name__,
-                ),
-            )
-
+            super(BigInt, self).__add__(other)
+        
         instance = self.__class__(name)
         instance._fields = {name: value}
-
+        
         return instance
+
+    def __add__(self, other):
+        self.__general_operation(other, '+')
 
 
 class Integer(BigInt):
